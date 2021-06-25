916c482c26a191ff9228d88fe80e25faf98baeee
Luca Nerlich <luca.nerlich@googlemail.com>
2021-06-25 17:16:55 +0200

    remove stats author

--- java/src/main/java/analyzer/models/Statistic.java
+++ java/src/main/java/analyzer/models/Statistic.java
@@ -16,47 +16,15 @@ import java.util.Objects;
 public class Statistic {
     
     // who receives which emoji and how often
-    
-    private class StatsAuthor {
-        private final String id;
-        private final String name;
-        
-        public StatsAuthor(String id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-        
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            StatsAuthor that = (StatsAuthor) o;
-            return id.equals(that.id) && name.equals(that.name);
-        }
-        
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, name);
-        }
-    }
-    
     private Map<Author, Map<Emoji, Integer>> authorEmojiMap = new HashMap<>();
     
     public void addAuthor(Author author) {
-//        final StatsAuthor statsAuthor = getStatsAuthor(author);
-        
         if (!authorEmojiMap.containsKey(author)) {
             authorEmojiMap.put(author, new HashMap<>());
         }
     }
     
     public void addEmoji(Author author, Emoji emoji, int count) {
-//        final StatsAuthor statsAuthor = getStatsAuthor(author);
-        
         if (!authorEmojiMap.containsKey(author)) {
             addAuthor(author, emoji, count);
         } else {
@@ -70,12 +38,7 @@ public class Statistic {
         }
     }
     
-    private StatsAuthor getStatsAuthor(Author author) {
-        return new StatsAuthor(author.getId(), author.getName());
-    }
-    
     private void addAuthor(Author author, Emoji emoji, int count) {
-//        final StatsAuthor statsAuthor = getStatsAuthor(author);
         addAuthor(author);
         authorEmojiMap.get(author).put(emoji, count);
     }
